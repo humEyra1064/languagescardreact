061fb5614a3639a94524cfdafd6a6d9574293047
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-02-16 13:33:15 +0300

    card added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-
-// import './App.css';
+import './App.css';
+import { Container } from 'react-bootstrap';
+import Header from "./components/Header";
+import Card from "./components/Card";
 
 
 
@@ -7,10 +9,14 @@ function App() {
    
   
   return (
-   
-   <div className="App">
+<Container className='text-center mt-4 p-4'> 
 
-   </div>
+  <Header/>
+  <Card/>
+  
+
+</Container>
+
 
   );
 }
